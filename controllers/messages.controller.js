c4f36175f7638cc7d9e9ccd6fb29bbc432dc41ae
Ales Pazniak <ales.pazniak90@gmail.com>
2022-11-29 22:44:31 +0300

    Templates, send files, MVC

--- controllers/messages.controller.js
+++ controllers/messages.controller.js
@@ -1,5 +1,8 @@
+const path = require('path')
+
 function getMessages(req, res) {
-    res.send('<b>Hello, Albert</b>')
+    // res.send('<b>Hello, Albert</b>')
+    res.sendFile(path.join(__dirname, '..', 'public', 'images', 'random.jpg'))
 }
 
 function postMessage(req, res) {
